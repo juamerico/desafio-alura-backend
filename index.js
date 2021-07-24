371bfe6531566847c179807d207897741ffa52bc
Juliana Américo <juliana.americo>
2021-07-23 23:19:47 -0300

    Porta dinâmica
    Node modules

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ db.connect(erro => {
 
         const app = customExpress()
 
-        const port = 4000
-        app.listen(port, () => {
-            console.log(`Server running on http://localhost:${port}`)
-        })
+        app.listen(process.env.PORT || 4000, () => console.log("rodando"))
     }
 })
