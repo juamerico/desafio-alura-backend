5fe4bb487f91f03d4b7bbb3d19039ad1becbf864
Juliana Américo <juliana.americo>
2021-07-24 01:49:51 -0300

    JAWS

--- infrastructure/dbConnection.js
+++ infrastructure/dbConnection.js
@@ -1,13 +1,15 @@
 const mysql = require("mysql")
 
 const connection = mysql.createConnection({
-    host: "us-cdbr-east-04.cleardb.com",
+    host: "kfgk8u2ogtoylkq9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "b28dba569e9a85",
-    password: "2220acc7",
-    database: "heroku_f13adb82a8f2201"
+    user: "mwylsvcbcj026b7z",
+    password: "r3fx1aihazlrdne4",
+    database: "d8v6lfvxjgxnf7xj"
 })
 
 module.exports = connection
 
 //mysql://b28dba569e9a85:2220acc7@us-cdbr-east-04.cleardb.com/heroku_f13adb82a8f2201?reconnect=true
+
+//mysql://mwylsvcbcj026b7z:r3fx1aihazlrdne4@kfgk8u2ogtoylkq9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/d8v6lfvxjgxnf7xj
