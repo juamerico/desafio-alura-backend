3f0a829ff8fdc57671931842cfd24b4e568561d1
Juliana Américo <juliana.americo>
2021-07-23 22:58:33 -0300

    Database

--- infrastructure/dbConnection.js
+++ infrastructure/dbConnection.js
@@ -1,11 +1,13 @@
 const mysql = require("mysql")
 
 const connection = mysql.createConnection({
-    host: "localhost",
+    host: "us-cdbr-east-04.cleardb.com",
     port: 3306,
-    user: "root",
-    password: "admin",
-    database: "aluraflix"
+    user: "b28dba569e9a85",
+    password: "2220acc7",
+    database: "heroku_f13adb82a8f2201"
 })
 
 module.exports = connection
+
+//mysql://b28dba569e9a85:2220acc7@us-cdbr-east-04.cleardb.com/heroku_f13adb82a8f2201?reconnect=true
